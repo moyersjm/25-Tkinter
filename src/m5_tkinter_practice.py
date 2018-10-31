db009256d4602034124f01fca395b39b242cb026
moyersjm <moyersjm@rose-hulman.edu>
2018-10-31 08:34:58 -0400

    All ToDos Done

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -2,8 +2,8 @@
 This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jonathan Moyers.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -12,28 +12,47 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI with stuff on it. """
     # ------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # Done: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # ------------------------------------------------------------------
+    root = tkinter.Tk()
+    frame1 = ttk.Frame(root, padding=100)
+    button1 = ttk.Button(frame1, text='Hello')
+    button1['command'] = lambda: print('Hello')
+    entry1 = ttk.Entry(frame1)
+    button2 = ttk.Button(frame1, text='Goodbye?')
+    button2['command'] = lambda: print(goodbye(entry1))
+    entry2 = ttk.Entry(frame1)
+    button3 = ttk.Button(frame1, text='Print')
+    button3['command'] = lambda: print_n_times(entry1, entry2)
+
+    frame1.grid()
+    button1.grid()
+    entry1.grid()
+    button2.grid()
+    entry2.grid()
+    button3.grid()
+
+    root.mainloop()
 
     # ------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # Done: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # ------------------------------------------------------------------
 
     # ------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # Done: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # ------------------------------------------------------------------
 
     # ------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # Done: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # ------------------------------------------------------------------
 
     # ------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # Done: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
@@ -42,7 +61,7 @@ def main():
     # ------------------------------------------------------------------
 
     # ------------------------------------------------------------------
-    # TODO: 7.
+    # Done: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -66,8 +85,20 @@ def main():
     ####################################################################
 
     # ------------------------------------------------------------------
-    # TODO: 8. As time permits, do other interesting GUI things!
+    # Done: 8. As time permits, do other interesting GUI things!
     # ------------------------------------------------------------------
+
+
+def goodbye(entry):
+    if entry.get() == 'ok':
+        return 'Hello'
+    else:
+        return 'Goodbye'
+
+
+def print_n_times(entry1, entry2):
+    for k in range(int(entry2.get())):
+        print(entry1.get(), end='')
 
 
 # ----------------------------------------------------------------------
